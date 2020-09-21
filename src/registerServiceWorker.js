13ae05137162ed9b1b7e18d1e92cb72154448263
朱磊 <lookenghua@gmail.com>
2020-09-21 15:09:00 +0800

    修改为中文提示

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -5,30 +5,25 @@ import { register } from "register-service-worker";
 if (process.env.NODE_ENV === "production") {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready() {
-      console.log(
-        "App is being served from cache by a service worker.\n" +
-          "For more details, visit https://goo.gl/AFskqB"
-      );
+      console.log("准备完成");
     },
     registered() {
-      console.log("Service worker has been registered.");
+      console.log("注册成功");
     },
     cached() {
-      console.log("Content has been cached for offline use.");
+      console.log("缓存");
     },
     updatefound() {
-      console.log("New content is downloading.");
+      console.log("正在下载新内容");
     },
     updated() {
-      console.log("New content is available; please refresh.");
+      console.log("新内容可用");
     },
     offline() {
-      console.log(
-        "No internet connection found. App is running in offline mode."
-      );
+      console.log("应用已脱机");
     },
     error(error) {
-      console.error("Error during service worker registration:", error);
+      console.error("服务注册失败:", error);
     }
   });
 }
